c9b311e722dafdb91097f36f6e5098ad9f1854c9
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-12-03 22:59:56 +0100

    update

--- db/db_running.ps1
+++ db/db_running.ps1
@@ -3,10 +3,12 @@ $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # 2. Définit le chemin absolu du fichier .env
 # Assurez-vous que le chemin est correct depuis $ScriptDir (ex: 'db/../src/WebApi/.env')
-$EnvFilePath = Join-Path $ScriptDir '../src/WebApi/.env'
+$RelativeEnv = Join-Path $ScriptDir '../src/WebApi/.env'
+$EnvFilePath = Resolve-Path $RelativeEnv
 
 # 3. Se déplace dans le répertoire du script
 cd $ScriptDir
 
+docker compose down -v
 # 4. Exécute la commande avec le chemin absolu du fichier .env
 docker compose --env-file $EnvFilePath up -d
